767305ad3a65f1c6290b3069d3ad65217bfd7761
Daniel Alves <alves.daniel4320@gmail.com>
2025-06-11 12:41:18 -0300

    feat: add main class

---
+++ src/com/daniel/jogodavelha/Main.java
@@ -0,0 +1,11 @@
+package com.daniel.jogodavelha;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new TelaPrincipal());
+    }
+
+}
